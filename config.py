2bcdcb3c4f1b707f8cf021679caefd3c9618e354
Midorichan <midorichan@adm.rspnet.jp>
2021-08-26 14:23:07 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,15 @@
+#Developer Portalから取得したBotのトークンを記入してください。ユーザートークンは使用しないでください。
 BOT_TOKEN = ""
+
+#BotのコマンドPrefixを設定します。
 PREFIX = ""
+
+#Botの所有者を設定します。ユーザーIDを入力してください。
 OWNER_ID = 
+
+#Googleから取得した、YouTube APIキーを入力してください。
+#音楽機能を使用する際にAPIキーが必須となります。
+YOUTUBE_KEY = ""
+
+#Embed(埋め込み)の枠の色を指定できます。16進数(hex)を入力してください。
+EMBED_COLOR = None
